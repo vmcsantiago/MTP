fbbeadfbad7df53e7b99fe84fe8c77d86d732750
Vilson Magner <vmcsantiago@gmail.com>
2016-06-28 12:04:07 -0300

    Update ControleDeAcesso.c

--- ControleDeAcesso.c
+++ ControleDeAcesso.c
@@ -1,21 +1,24 @@
-/* Software para controle de acesso com ArduÌno
+/* Software para controle de acesso com Ardu√≠no
 Aluno: Vilson Magner Carrijo Santiago
-N∫ matrÌcula: 11521EEL011 */
+N¬∫ matr√≠cula: 11521EEL011 */
 
 #include <stdio.h>
 #include <windows.h>
 #include <locale.h>
 #include <string.h>
+#include <stdlib.h>
 #define PADRAO 2108 //Defina a senha do administrador para realizar novos cadastros
 
-void menu() // FunÁ„o que mostra o menu principal
+void menu() // Fun√ß√£o que mostra o menu principal
 {
     system("cls");
-    printf("------------- Projeto MTP # Controle de Acesso com ArduÌno -------------\n\n\n");
+    printf("------------- Projeto MTP # Controle de Acesso com Ardu√≠no -------------\n\n\n");
     printf("[1] Cadastro de login/senha\n");
     printf("[2] Digitar login/senha para liberar acesso\n");
-    printf("[3] Sair do programa\n\n");
-    printf("OpÁ„o desejada: ");
+    printf("[3] Conferir o n√∫mero de acessos do dia\n");
+    printf("[4] Abrir relat√≥rio.txt com login/senha dos acessos do dia\n");
+    printf("[5] Sair do programa\n\n");
+    printf("Op√ß√£o desejada: ");
 
 }
 
@@ -39,6 +42,8 @@ typedef struct
 elemento *F = NULL;
 elemento *Atual = NULL;
 elemento *Anterior = NULL;
+int acessos = 0;
+FILE *relatorio;
 
 elemento *alocarNo()
 {
@@ -51,12 +56,15 @@ elemento *alocarNo()
 void conferir(int user, int pass, HANDLE hCom)
 {
     int encontrou = 0;
+    relatorio = fopen("relatoriodeacessos.txt", "a");
+
+
 
     if(F==NULL) //FILA VAZIA
     {
-        printf("Nenhum usu·rio cadastrado!\n");
+        printf("Nenhum usu√°rio cadastrado!\n");
     }
-    else // FILA N√O EST¡ VAZIA
+    else // FILA N√ÉO EST√Å VAZIA
     {
         Atual = F;
 
@@ -65,6 +73,9 @@ void conferir(int user, int pass, HANDLE hCom)
             if(Atual->usuario == user && Atual->senha == pass)
             {
                 encontrou = 1;
+                fprintf(relatorio, "Usu√°rio: %i - Senha: %i\n",Atual->usuario,Atual->senha);
+                fclose(relatorio);
+                break;
             }
             Atual = (elemento *)Atual->prox;
         }
@@ -74,13 +85,16 @@ void conferir(int user, int pass, HANDLE hCom)
     {
         printf("\nAcesso permitido!\n");
         SerialPutc(hCom, '1');
+        acessos++;
         system("pause");
     }
     else
     {
         printf("\nAcesso negado!\n");
+        SerialPutc(hCom, '0');
         system("pause");
     }
+    fclose(relatorio);
 }
 
 void logar(HANDLE hCom)
@@ -90,7 +104,7 @@ void logar(HANDLE hCom)
     int pass;
 
     printf("Seja bem-vindo ao login para o acesso!\n\n");
-    printf("Insira o usu·rio: ");
+    printf("Insira o usu√°rio: ");
     scanf("%d",&user);
     printf("Insira a senha: ");
     scanf("%d",&pass);
@@ -111,9 +125,9 @@ void cadastrar()
 
     if(op == PADRAO)
     {
-        printf("Digite o usu·rio (NumÈrico): ");
+        printf("Digite o usu√°rio (Num√©rico): ");
         scanf("%d",&user);
-        printf("Digite a senha (NumÈrico): ");
+        printf("Digite a senha (Num√©rico): ");
         scanf("%d",&pass);
 
         if(F == NULL) // Fila vazia
@@ -122,7 +136,7 @@ void cadastrar()
             F->usuario = user;
             F->senha = pass;
         }
-        else // Fila n„o est· vazia
+        else // Fila n√£o est√° vazia
         {
             Atual = F;
 
@@ -147,7 +161,7 @@ void cadastrar()
         }
     }
     else
-        printf("VocÍ n„o tem permiss„o para efetuar um novo cadastro!\n");
+        printf("Voc√™ n√£o tem permiss√£o para efetuar um novo cadastro!\n");
 
     system("pause");
 }
@@ -155,11 +169,18 @@ void cadastrar()
 void main() // Programa principal
 {
     int opcao;
+    char dateStr[9];
+    int passadm;
     setlocale(LC_ALL, "Portuguese");
     DCB dcb;
     HANDLE hCom;
     BOOL fSuccess;
     LPCWSTR LpcCommPort = "COM3";
+
+    relatorio = fopen("relatoriodeacessos.txt","w");
+    _strdate(dateStr);
+    fprintf(relatorio, "Controle de Acesso - Relat√≥rio do login/senha dos acessos do dia (mm/dd/aa): %s\n\n",dateStr);
+    fclose(relatorio);
 
     hCom = CreateFile( LpcCommPort,
                     GENERIC_READ | GENERIC_WRITE,
@@ -209,7 +230,7 @@ void main() // Programa principal
     menu();
     scanf("%d",&opcao);
 
-    while(opcao != 3)
+    while(opcao != 5)
     {
         switch(opcao)
         {
@@ -219,8 +240,27 @@ void main() // Programa principal
             case 2:
                 logar(hCom);
                 break;
+            case 3:
+                system("cls");
+                printf("Seja bem-vindo ao controle do n√∫mero de acessos do dia (mm/dd/aa): %s\n\n",dateStr);
+                printf("Hoje tivemos %d acessos!\n",acessos);
+                system("pause");
+                break;
+            case 4:
+                printf("Digite a senha do administrador: ");
+                scanf("%i",&passadm);
+                if(passadm == PADRAO)
+                {
+                    system("relatoriodeacessos.txt");
+                }
+                else
+                {
+                    printf("Voc√™ n√£o tem permiss√£o para visualizar o relat√≥rio de acessos!\n");
+                    system("pause");
+                }
+                break;
             default:
-                printf("OpÁ„o nao cadastrada, por favor, tente novamente!\n");
+                printf("Op√ß√£o nao cadastrada, por favor, tente novamente!\n");
                 system("pause");
         }
         menu();
